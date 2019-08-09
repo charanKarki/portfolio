5777bc8f68f2fc84169aa6f61fb8be4a1c76bba2
charan karki <charankarki.31@gmail.com>
2019-08-09 08:54:11 +0530

    file added

---
+++ js/script.js
@@ -0,0 +1,46 @@
+
+
+$(document).ready(function () {
+    // navbar
+    $('#navbarId').on('hide.bs.collapse ',function(){$('#navbarId').slideUp()})
+    $('#navbarId').on('show.bs.collapse ',function(){$('#navbarId').slideDown()})
+
+
+    window.onscroll = function(){
+        if($('.navbar').offset().top<=50){
+            $('.navbar').removeClass(' bg-light').addClass('navbar-dark')
+            $('#navbarId .nav-item .nav-link ,.navbar-brand').css('color','white')
+
+        }
+        else{
+            $('.navbar').addClass('navbar-light bg-light').removeClass('navbar-dark')
+            $('#navbarId .nav-item .nav-link,.navbar .navbar-brand').css('color','#06BCC1')
+
+        }
+    }
+
+    // blog slider
+    $('#blog-slider').slick({
+        infinite:true,
+        slidesToShow:3,
+        responsive:[
+            {
+                breakpoint:768,
+                settings:{
+                slidesToShow:1
+
+                }
+            }
+        ]    })
+
+
+        // scroll to top
+        $(".scroll-to-top").click(function(){
+            window.scroll({
+                top: 0, 
+                left: 0, 
+                behavior: 'smooth'
+              });
+            
+        })
+});
